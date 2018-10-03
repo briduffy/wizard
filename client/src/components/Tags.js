0ca22fce3933aad45b81c4ba4ed655f0e140d6a5
Brianne Duffy <duffymbrianne@gmail.com>
2018-10-03 11:22:46 -0600

    delete tags

--- client/src/components/Tags.js
+++ client/src/components/Tags.js
@@ -1,15 +1,65 @@
 import React from 'react'
-import { Container, Header, Segment, Divider } from 'semantic-ui-react'
+import { 
+  Container, 
+  List,
+  Header,
+  Divider,
+  Segment,
+} from 'semantic-ui-react'
+import { connect } from 'react-redux'
 import TagForm from './TagForm'
+import { getTags, deleteTag } from '../reducers/tags'
 
-const Tags = () => (
-  <Container>
-    <Divider hidded />
-    <Segment padded>
-      <Header as='h3'>Create a Video Game Tag</Header>
-      <TagForm />
-    </Segment>
-  </Container>
-)
+class Tags extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(getTags())
+  }
 
-export default Tags
+  render() {
+    const { tags, dispatch } = this.props
+
+    return (
+      <Container>
+        <Divider />
+          <Segment padded>
+            <Header as='h3'>Create a Video Game Tag</Header>
+              <TagForm />
+          </Segment>
+            { tags.length > 0 &&
+                <>
+                  <Header as="h3" textAlign="center">Tags</Header>
+                  <List divided horizontal>
+                    { tags.map( tag => {
+                        const { id, name } = tag
+                        return (
+                          <List.Item 
+                            key={id} 
+                            style={{ backgroundColor: 'aliceblue' }}
+                          >
+                            <List.Icon 
+                              name="cancel" 
+                              style={{ cursor: 'pointer' }}
+                              onClick={ () => dispatch(deleteTag(id))}
+                            />
+                            <List.Content>
+                              <List.Header>
+                                #{name}
+                              </List.Header>
+                            </List.Content>
+                          </List.Item>
+                        )
+                      })
+                    }
+                  </List>
+                </>
+            }
+      </Container>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { tags: state.tags }
+}
+
+export default connect(mapStateToProps)(Tags)
